0bd4004040cd1442c5ed2fc19e52c72b08ae873e
Caizhen Liu <czliu@CNczliu.local>
2016-10-18 14:46:32 +0800

    init project

---
+++ spec/bubble_sort/bubble_sort_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../spec_helper'
+require_relative '../../app/bubble_sort/bubble_sort'
+
+RSpec.describe BubbleSort do
+  it 'should sort [3, 1, 2] to [1, 2, 3]' do
+    expect(true).to eq(true)
+  end
+end
