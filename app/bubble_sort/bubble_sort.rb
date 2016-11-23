a3c7f9622b3a93e39602a74023ef6527fd452f23
zhen639 <zhen6939@gmail.com>
2016-11-23 10:42:05 +0800

    implement bubble sort

--- app/bubble_sort/bubble_sort.rb
+++ app/bubble_sort/bubble_sort.rb
@@ -1,3 +1,14 @@
 module  BubbleSort
-
+  def self.sort(arr)
+    (0 .. arr.length).each do |order|
+      (0 .. order).each do |label|
+        if arr[label+1] && arr[label] > arr[label+1]
+          temp = arr[label+1]
+          arr[label+1] = arr[label]
+          arr[label] = temp
+        end
+      end
+    end
+    arr
+  end
 end
--- spec/bubble_sort/bubble_sort_spec.rb
+++ spec/bubble_sort/bubble_sort_spec.rb
@@ -3,6 +3,8 @@ require_relative '../../app/bubble_sort/bubble_sort'
 
 RSpec.describe BubbleSort do
   it 'should sort [3, 1, 2] to [1, 2, 3]' do
-    expect(true).to eq(true)
+    arr = %w(3 1 2)
+    result = BubbleSort.sort(arr)
+    expect(result).to eq(%w(1 2 3))
   end
 end
